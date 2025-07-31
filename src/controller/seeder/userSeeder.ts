59b3d328fa30b857f81f39c64a6429b8236306bd
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-07-31 14:28:55 +0700

    feat : new endpoint for user seeder with docmentation in request rest file

---
+++ src/controller/seeder/userSeeder.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../../data-source";
+import { user,UserRole } from "../../model/user";
+import { encrypt } from "../../utils/CryptoData";
+
+const { successResponse, errorResponse, validationResponse } = require('../../utils/response')
+
+
+const userRepository = AppDataSource.getRepository(user)
+
+
+export const userSeeder = async (req: Request, res: Response) => {
+    const userSeed = [
+        {userName : "Admin1",password : "Admin123!"},
+    ];
+    try{
+        for (const data of userSeed){
+            const newUser = new user()
+            newUser.userName = data.userName
+            newUser.password =  data.password
+            newUser.hashPassword()
+            await userRepository.save(newUser)
+        }
+        console.log("User seeded successfully.");
+
+    }catch(error){
+        return res.status(400).send(errorResponse(error,400))
+    }
+}
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import SeederRoute from './SeederRoute'
 
 
 
 const router = Router()
+router.use('/seeder',SeederRoute)
+
 
 export default router
